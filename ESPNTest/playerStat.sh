b144c954bacfcba3427da0df354a214523c3e29c
connorruff <cruff@nd.edu>
2020-02-14 10:47:42 -0500

    Feb14

--- ESPNTest/playerStat.sh
+++ ESPNTest/playerStat.sh
@@ -5,4 +5,8 @@ FIRST=$1
 LAST=$2
 URL=$3
 
-echo "points: " $(sh getPts.sh $1 $2 $3) 
+echo "Points: " $(sh getPts.sh $1 $2 $3)
+echo "Assists: " $(sh getAst.sh $1 $2 $3)
+echo "Rebounds: " $(sh getReb.sh $1 $2 $3)
+echo "Steals: " $(sh getStls.sh $1 $2 $3)
+echo "Blocks: " $(sh getBlks.sh $1 $2 $3)
