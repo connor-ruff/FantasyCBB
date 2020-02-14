ff5afc8b821c158514d3b14dd9716c25646009c7
connorruff <cruff@nd.edu>
2020-02-14 16:27:47 -0500

    Full Game Stat v0.0

--- StatGrabbing/README.md
+++
@@ -1,4 +0,0 @@
-# Notes To Consider
-
--Issue: replacing the - with a space for the player names will
-		result in a bug for players with hyphenated names
--- StatGrabbing/allNames.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-URL=$1
-
-curl -sL $URL | grep -Eo "player/_/id/[[:digit:]]+/[[:alpha:]]+-[[:alpha:]]+\"" | cut -d / -f5 | tr -d \" | sed -E 's/-/ /g'
-
